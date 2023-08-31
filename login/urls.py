34711b2da38c42c8216762218a0246b44ada5f13
AhmedDR200 <alshwwhy212@gmail.com>
2023-08-31 19:38:16 +0300

    start new app (guide)

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import register_user, user_login, user_logout
-from login import views
+from .views import register_user, user_login, user_logout , get_all_users , user_detail
+
 
 
 
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-    path('all_users', views.get_all_users, name='get_all_users'),
-    path('users/<int:user_id>/', views.user_detail, name='user-detail'),
+    path('all_users', get_all_users, name='get_all_users'),
+    path('users/<int:user_id>/', user_detail, name='user-detail'),
 ]
