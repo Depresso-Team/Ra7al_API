3eef1f71b5dba30144d71e3e8ab13f742ff1bc4e
demo-23home <zeyadslama23@gmail.com>
2023-09-07 23:26:22 +0300

    linking Database to PostgreSQL

--- project/settings.py
+++ project/settings.py
@@ -90,11 +90,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'ra7al_db',
+        'USER':'postgres',
+        'PASSWORD':'toor',
+        'HOST':'localhost',
+        'PORT':5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
