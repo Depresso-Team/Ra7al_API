5330772a6eba1c1c91215b5c7f7b446ae7ba9202
AhmedDR200 <alshwwhy212@gmail.com>
2023-09-08 21:34:39 +0300

    test

--- manage.py
+++ manage.py
@@ -20,3 +20,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+from django.core.management import execute_from_command_line
+import sys
+
+if __name__ == "__main__":
+    execute_from_command_line(sys.argv)
