fbb60af1be763ab0a8456e1ace567e56aa749958
AhmedDR200 <alshwwhy212@gmail.com>
2023-09-12 13:03:39 +0300

    password validetors

--- login/serializers.py
+++ login/serializers.py
@@ -1,24 +1,31 @@
 from rest_framework import serializers
 from .models import CustomUser , Guide
+from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
+
 
 # User
+CustomUser = get_user_model()
+
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'password', 'phone', 'address', 'country_code', 'photo_url', 'languages', 'is_guide', 'session_message']
-        extra_kwargs = {'password': {'write_only': True}}  # Password is write-only in API
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+        return value
 
     def create(self, validated_data):
-        # Create a new CustomUser instance using validated data
         user = CustomUser(**validated_data)
-
-        # Set the password using the set_password method to ensure it's securely hashed
         user.set_password(validated_data['password'])
-
-        # Save the user object to the database
         user.save()
-
         return user
+
 
 
 
